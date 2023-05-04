4ca28fcb2779958b9a7f2a0a5a051c020865c474
iotassss <shokutaa@gmail.com>
2023-05-04 23:48:37 +0900

    tmp

--- index.js
+++ index.js
@@ -4,13 +4,23 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require('socket.io');
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: ['http://localhost:3000'],
+  },
+});
 
 const PORT = 5001;
 
 // クライアントと通信
 io.on('connection', (socket) => {
   console.log('a user connected');
+
+  // クライアントからの受信
+  socket.on('send_message', (data) => {
+    console.log(data);
+  });
+
   socket.on('disconnect', () => {
     console.log('disconnect');
   });
